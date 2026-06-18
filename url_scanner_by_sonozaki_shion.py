425c332d932f8bef08abc9b5c49cc9f2a816a24c
DiegoDev <diegorego516@gmail.com>
2026-06-18 18:31:11 -0300

    Add files via upload

---
+++ url_scanner_by_sonozaki_shion.py
@@ -0,0 +1,31 @@
+import requests
+import socket
+
+print("\nURL_SCANNER_BY_SONOZAKI_SHION - V7.2.9")
+r = requests.get("https://desmos.com/")
+r.status_code = 200
+r.connection.proxy_headers
+scan_url = input("url = https://desmos.com/")
+if scan_url in scan_url:
+    input("Enter the url of the website that you want to scan for vulnerabilties: ")
+if not scan_url in scan_url:
+    input("That URL you entered is incorrect, try again? (Y/N): ")
+try:
+    input("Enter the correct URL link this time: ")
+except KeyboardInterrupt as e:
+    input("What kind of URL should you add? Enter here the answer: ")
+r.encoding = scan_url
+socket.create_connection = ("WEBSITE_URL, TARGET_TRACEROUTE")
+socket.create_server = ("TARGET_IP, WEBSITE_SCANNER")
+print("[!] Warning: Verifying the website for potential vulnerabilities...")
+print("[-] No vulnerabilities found. Want to try other website?")
+print("[+] Do you want to scan another website?")
+int = input("[+] Do you want to verify the website for potential vulnerabilities? (Y/N): ")
+int = input("[-] No vulnerabilities has been found... do you want to try another website? (Y/N): ")
+int = input("[!] Warning: Active Scan has started on another website... Press CTRL+C to stop the scan: ")
+print("[!] Scan has stopped midway, resulting in broken or failed scans.")
+print("[!] Do you want to re-run the scan on another website?")
+int = input("[+] Enter what response you want to answer: ")
+int = input("[*] Enter up the URL of the website here? (Y/N): ")
+int = input("[+] Enter here the website URL that you want to do vulnerability testing: ")
+print("[!] Testing up the vulnerability of the website URL.")
